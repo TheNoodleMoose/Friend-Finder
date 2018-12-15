ce22999e583f967c9d281affc8ecf7cab4e7b187
TheNoodleMoose <cjh605@gmail.com>
2018-12-15 14:08:51 -0500

    Adding initial files

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+require("./app/routing/apiRoutes.js")(app);
+require("./app/routing/htmlRoutes.js")(app);
+
+app.listen(PORT, () => {
+  console.log("App listening on PORT " + PORT);
+});
