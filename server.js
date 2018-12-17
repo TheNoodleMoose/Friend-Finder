e0b7e22f05d023f5f11ab32e212e2465e0d8dadb
TheNoodleMoose <cjh605@gmail.com>
2018-12-17 10:53:05 -0500

    Changed Styling

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static("app/public/"));
 
 require("./app/routing/apiRoutes.js")(app);
 require("./app/routing/htmlRoutes.js")(app);
